8cc299a782b68ea757813d85869fdcf4d72295d2
Oliver Müske <info@oliver-mueske.de>
2020-06-15 21:10:48 +0200

    Year Color: Added Errorhandling and Logging

--- app/controllers/yearColor.controller.js
+++ app/controllers/yearColor.controller.js
@@ -1,27 +1,38 @@
 exports.showColor = (req, res) => {
-  let result = req.params.year % 5;
-  let yearColor;
+  const logger = require("../config/winston");
 
-  switch (result) {
-    case 0:
-      yearColor = "blue";
-      break;
-    case 1:
-      yearColor = "white";
-      break;
-    case 2:
-      yearColor = "yellow";
-      break;
-    case 3:
-      yearColor = "red";
-      break;
-    case 4:
-      yearColor = "green";
-      break;
+  if (!req.params.year) {
+    logger.error("HTTP-400: 'year' cannot be empty");
+    res.status(400).send({ message: "year cannot be empty" });
+  } else {
+    let result = req.params.year % 5;
+    let yearColor;
+
+    logger.info("yearColor called. Year: " + req.params.year);
+    switch (result) {
+      case 0:
+        yearColor = "blue";
+        break;
+      case 1:
+        yearColor = "white";
+        break;
+      case 2:
+        yearColor = "yellow";
+        break;
+      case 3:
+        yearColor = "red";
+        break;
+      case 4:
+        yearColor = "green";
+        break;
+    }
+    logger.info("Calculated Color: " + yearColor);
+
+    logger.info("HTTP-200: Year Color calculated");
+    return res.status(200).send({
+      success: "true",
+      message: "Color calculated successfully",
+      yearColor,
+    });
   }
-  return res.status(200).send({
-    success: "true",
-    message: "Color calculated successfully",
-    yearColor,
-  });
 };
